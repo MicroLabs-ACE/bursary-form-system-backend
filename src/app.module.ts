2a7a96b3689746d124d6dd702063f4ac17395298
Victor Momodu <victoramomodu@gmail.com>
2024-09-12 12:11:58 +0100

    refactor: Prefix HOST and POST env variables with POSTGRES_

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,8 +16,8 @@ import { UsersModule } from './users/users.module';
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
         type: 'postgres',
-        host: configService.get<string>('HOST'),
-        port: +configService.get<string>('PORT'),
+        host: configService.get<string>('POSTGRES_HOST'),
+        port: +configService.get<string>('POSTGRES_PORT'),
         username: configService.get<string>('POSTGRES_USER'),
         password: configService.get<string>('POSTGRES_PASSWORD'),
         database: configService.get<string>('POSTGRES_DB'),
