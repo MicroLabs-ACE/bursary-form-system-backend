90ab7b8d57e1cedb8d3f7342a5aa73252ddd3c51
Victor Momodu <victoramomodu@gmail.com>
2024-10-15 15:02:25 +0100

    feat: Allow headers for tokens.

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ credentials: true });
+  app.enableCors({
+    allowedHeaders: ['Content-Type', 'X-Access-Token', 'X-Refresh-Token'],
+  });
   const configService = app.get(ConfigService);
   const config = new DocumentBuilder()
     .setTitle('Bursary Form Service')
